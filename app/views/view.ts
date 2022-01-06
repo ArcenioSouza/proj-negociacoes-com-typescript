3058e5b28ee15e3cbed3955878878f1bbac11fc7
Arcenio Souza <arcenio_neto@icloud.com>
2022-01-06 12:08:20 -0300

    Finaliza projeto com typescript

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,7 +5,12 @@ export abstract class View<T> {
    protected elemento: HTMLElement
 
    constructor(seletor: string){
-      this.elemento = document.querySelector(seletor)
+      const elemento = document.querySelector(seletor)
+      if(elemento){
+         this.elemento = elemento as HTMLElement
+      } else {
+         throw Error (`Seletor ${seletor} não existe no DOM. Verifique`)
+      }
    }
 
    public update(model: T): void {
